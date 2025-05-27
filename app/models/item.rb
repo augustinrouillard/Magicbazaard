78f6ebc42412224096110991d05b85c8afc08e37
Matis Martin <matis.martin@outlook.fr>
2025-05-27 14:44:01 +0200

    Item controller + Cloudinary

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   CATEGORY = %w(baguette chaudron amulette balais cape)
   POWER = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-         has_one_attached :image
+  has_one_attached :image
 
 
   belongs_to :user
