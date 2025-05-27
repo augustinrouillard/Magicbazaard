18447aeacdf897b9f7d4c7bfd6936a4da79d0a8e
jean mosnier <jean.mosnier92@gmail.com>
2025-05-27 13:00:24 +0200

    controller comment done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def profile
+    @user = User.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.where(target_id: @user.id)
+  end
 end
