0e486e1ef48ef711dd367c26c9bc391fcba126f2
clement garreau <garreau.clem03@gmail.com>
2025-05-28 13:03:52 +0200

    affiche ce quil faut

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  scope :past, -> { where('ending_date < ?', Date.today) }
+  scope :current, -> { where('starting_date <= ? AND ending_date >= ?', Date.today, Date.today) }
+  scope :upcoming, -> { where('starting_date > ?', Date.today) }
 end
