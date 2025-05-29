4f1948174e1a9d8847f6ecf9a977cf302a3310c1
clement garreau <garreau.clem03@gmail.com>
2025-05-29 16:25:16 +0200

    reparation OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,15 +5,14 @@ class PagesController < ApplicationController
   end
 
   def profile
-    @user = current_user
-    @user1 = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @comment = Comment.new
-    @comments = Comment.where(target_id: @user1.id)
+    @comments = Comment.where(target_id: @user.id)
     allrating = @comments.map(&:rating)
     if allrating.count.zero? || allrating.all?(&:nil?)
-      @user1.rating = 0
+      @user.rating = 0
     else
-      @user1.rating = allrating.sum.to_f / allrating.size
+      @user.rating = allrating.sum.to_f / allrating.size
     end
   end
 end
