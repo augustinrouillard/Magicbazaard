c7a238271014fa07a860c2d8476476888ae0572c
jean mosnier <jean.mosnier92@gmail.com>
2025-05-30 10:29:13 +0200

    added items to public profile and style to form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :target, class_name: 'User', foreign_key: 'target_id'
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  RATING = [0, 1, 2, 3, 4, 5]
 end
